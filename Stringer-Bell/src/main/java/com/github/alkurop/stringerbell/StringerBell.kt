5fcb8205ac607114a0c6902c0a1adc09b7453d17
Alexey Kuropiatnyk <alexey.kuropiantnyk@ah.nl>
2021-03-04 19:02:19 +0100

    Split to OfString and OfRes

--- Stringer-Bell/src/main/java/com/github/alkurop/stringerbell/StringerBell.kt
+++ Stringer-Bell/src/main/java/com/github/alkurop/stringerbell/StringerBell.kt
@@ -3,11 +3,22 @@ package com.github.alkurop.stringerbell
 import android.content.Context
 import androidx.annotation.StringRes
 
-class Stringer(
-    @StringRes private val res: Int,
-    private vararg val params: Any
-) {
-    fun bell(context: Context): String {
-        return context.getString(res, *params)
+sealed class Stringer {
+    abstract fun bell(context: Context): String
+
+    class OfString(private val string: String) : Stringer() {
+        override fun bell(context: Context): String {
+            return string
+        }
+    }
+
+    class OfRes(
+        @StringRes private val res: Int,
+        private vararg val params: Any
+    ) : Stringer() {
+        override fun bell(context: Context): String {
+            return context.getString(res, *params)
+        }
     }
 }
+
--- app/src/main/kotlin/com/example/myapplication/MainActivity.kt
+++ app/src/main/kotlin/com/example/myapplication/MainActivity.kt
@@ -9,7 +9,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<TextView>(R.id.preview).text = Stringer(R.string.hello_world).bell(this)
-        findViewById<TextView>(R.id.preview_params).text = Stringer(R.string.hello_param_string_name, "Alex", 86).bell(this)
+        findViewById<TextView>(R.id.preview).text = Stringer.OfRes(R.string.hello_world).bell(this)
+        findViewById<TextView>(R.id.preview_params).text =
+            Stringer.OfRes(R.string.hello_param_string_name, "Alex", 86).bell(this)
+        findViewById<TextView>(R.id.preview_of_string).text = Stringer.OfString("Go Home").bell(this)
     }
 }
