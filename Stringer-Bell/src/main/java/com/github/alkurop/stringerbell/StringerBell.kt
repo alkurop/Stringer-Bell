a9850291db43767ec5f13df2c045ab8a09ce31c3
Alexey Kuropiatnyk <alexey.kuropiantnyk@ah.nl>
2021-03-04 19:52:40 +0100

    Simplify the ofstring

--- Stringer-Bell/src/main/java/com/github/alkurop/stringerbell/StringerBell.kt
+++ Stringer-Bell/src/main/java/com/github/alkurop/stringerbell/StringerBell.kt
@@ -3,22 +3,15 @@ package com.github.alkurop.stringerbell
 import android.content.Context
 import androidx.annotation.StringRes
 
-sealed class Stringer {
-    abstract fun bell(context: Context): String
-
-    class OfString(private val string: String) : Stringer() {
-        override fun bell(context: Context): String {
-            return string
-        }
+class Stringer(
+    @StringRes private val res: Int,
+    private vararg val params: Any
+) {
+    fun bell(context: Context): String {
+        return context.getString(res, *params)
     }
 
-    class OfRes(
-        @StringRes private val res: Int,
-        private vararg val params: Any
-    ) : Stringer() {
-        override fun bell(context: Context): String {
-            return context.getString(res, *params)
-        }
+    companion object {
+        fun ofString(string: String) = Stringer(R.string.sb_placeholder, string)
     }
 }
-
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,9 +9,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<TextView>(R.id.preview).text = Stringer.OfRes(R.string.hello_world).bell(this)
-        findViewById<TextView>(R.id.preview_params).text =
-            Stringer.OfRes(R.string.hello_param_string_name, "Alex", 86).bell(this)
-        findViewById<TextView>(R.id.preview_of_string).text = Stringer.OfString("Go Home").bell(this)
+        findViewById<TextView>(R.id.preview).text = Stringer(R.string.hello_world).bell(this)
+        findViewById<TextView>(R.id.preview_params).text = Stringer(R.string.hello_param_string_name, "Alex", 86).bell(this)
+        findViewById<TextView>(R.id.preview_of_string).text = Stringer.ofString("Go Home").bell(this)
     }
 }
